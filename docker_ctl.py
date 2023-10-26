0d495325ff5993b1225603560601cf95453a1c99
Huang Tao <69231000+TaoHuang99@users.noreply.github.com>
2023-10-26 17:40:47 +0800

    Update docker_ctl.py

--- docker_ctl.py
+++ docker_ctl.py
@@ -2,11 +2,40 @@ from flask import Flask, jsonify
 from flask import request
 import docker
 import subprocess
-
+import paramiko
+import os
 app = Flask(__name__)
 client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+def copy_file_to_remote(target_host, target_username, target_password, local_path, target_path):
+    try:
+        # 创建SSH客户端
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(target_host, username=target_username, password=target_password)
+        
+        # 创建SFTP会话
+        sftp = ssh.open_sftp()
+        _copy_folder_recursive(local_path, target_path, sftp)
+        sftp.close()
+        ssh.close()
+        return True, "File copied successfully!"
+    except Exception as e:
+        return False, str(e)
 
-script_path = "/home/admin/piskes_file/docker_run/docker_run.sh"
+def _copy_folder_recursive(local_path, target_path, sftp):
+    if os.path.isfile(local_path):
+        sftp.put(local_path, target_path)
+    elif os.path.isdir(local_path):
+        try:
+            sftp.mkdir(target_path)
+        except:
+            pass  # 忽略错误，如果文件夹已存在
+        for item in os.listdir(local_path):
+            local_item = os.path.join(local_path, item)
+            target_item = os.path.join(target_path, item)
+            _copy_folder_recursive(local_item, target_item, sftp)
+
+script_path = "/home/ht/piskes_file/docker_run/docker_run.sh"
 
 @app.route("/containers/<container_name>/stop", methods=['POST'])
 def stop_container(container_name):
@@ -22,31 +51,31 @@ def start_container(container_name):
     try:
         container = client.containers.get(container_name)
         
-        # 检查容器是否已经在运行
         if container.status == "running":
             return jsonify({'message': f'Container {container_name} is already running!'}), 304
         
-        # 定义目标主机的相关信息
-        # 获取JSON数据
         data = request.get_json()
         if not data or 'target_host' not in data:
             return jsonify({'error': 'Missing target_host in request data'}), 400
         
         target_host = data['target_host']
-
         target_username = "admin"
         target_password = "123"
-        target_path = "/home/admin/piskes_file"
+        target_path = "/home/admin"
         local_path = "/home/admin/piskes_file"
         
-        # 将文件复制到目标主机
-        if copy_file_to_remote(target_host, target_username, target_password, local_path, target_path):
-            return jsonify({'message': 'File copied successfully!'}), 204
+        success, message = copy_file_to_remote(target_host, target_username, target_password, local_path, target_path)
+        if success:
+            return jsonify({'message': message}), 204
         else:
-            return jsonify({'error': 'Failed to copy file'}), 500
+            app.logger.error('Failed to copy file: %s', message)
+            return jsonify({'error': message}), 500
 
     except Exception as e:
+        app.logger.error('Error in start_container: %s', str(e))
         return jsonify({'error': str(e)}), 500
+
+
 
 
 
