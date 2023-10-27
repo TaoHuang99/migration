89bc704e31348c689ab0cf802fbf3baac4626a1e
Huang Tao <69231000+TaoHuang99@users.noreply.github.com>
2023-10-27 18:05:36 +0800

    Update docker_ctl.py

--- docker_ctl.py
+++ docker_ctl.py
@@ -4,46 +4,10 @@ import docker
 import subprocess
 import paramiko
 import os
-path = "/home/admin/piskes_file"
-os.chmod(path, 0o777)
+
 app = Flask(__name__)
 client = docker.DockerClient(base_url='unix://var/run/docker.sock')
-def copy_file_to_remote(target_host, target_username, target_password, local_path, target_path):
-    try:
-        # 创建SSH客户端
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(target_host, username=target_username, password=target_password)
-        
-        # 创建SFTP会话
-        sftp = ssh.open_sftp()
-        _copy_folder_recursive(local_path, target_path, sftp)
-        
-        # 设置目标路径的权限
-        stdin, stdout, stderr = ssh.exec_command(f"chmod -R 777 {target_path}")
-        stderr = stderr.read().decode()
-        if stderr:
-            return False, f"Failed to set permissions on remote path: {stderr}"
-        
-        sftp.close()
-        ssh.close()
-        return True, "File copied and permissions set successfully!"
-    except Exception as e:
-        return False, str(e)
 
-
-def _copy_folder_recursive(local_path, target_path, sftp):
-    if os.path.isfile(local_path):
-        sftp.put(local_path, target_path)
-    elif os.path.isdir(local_path):
-        try:
-            sftp.mkdir(target_path)
-        except:
-            pass  # 忽略错误，如果文件夹已存在
-        for item in os.listdir(local_path):
-            local_item = os.path.join(local_path, item)
-            target_item = os.path.join(target_path, item)
-            _copy_folder_recursive(local_item, target_item, sftp)
 
 script_path = "/home/admin/piskes_file/docker_run/docker_run.sh"
 
@@ -65,21 +29,19 @@ def start_container(container_name):
             return jsonify({'message': f'Container {container_name} is already running!'}), 304
         
         data = request.get_json()
-        if not data or 'target_host' not in data:
-            return jsonify({'error': 'Missing target_host in request data'}), 400
+        if not data or 'KeyServerDomain' not in data:
+            return jsonify({'error': 'Missing KeyServerDomain in request data'}), 400
         
-        target_host = data['target_host']
-        target_username = "admin"
-        target_password = "123"
-        target_path = "/home/admin/piskes_file"
-        local_path = "/home/admin/piskes_file"
-        
-        success, message = copy_file_to_remote(target_host, target_username, target_password, local_path, target_path)
-        if success:
-            return jsonify({'message': message}), 204
-        else:
-            app.logger.error('Failed to copy file: %s', message)
-            return jsonify({'error': message}), 500
+        # 更新config.ini文件
+        config = configparser.ConfigParser()
+        config.read('/home/song/piskes_file/piskes/config/config.ini')
+        config['DEFAULT']['KeyServerDomain'] = data['KeyServerDomain']
+        with open('/home/song/piskes_file/piskes/config/config.ini', 'w') as configfile:
+            config.write(configfile)
+
+
+
+        return jsonify({'message': 'Configuration updated and container started successfully!'}), 204
 
     except Exception as e:
         app.logger.error('Error in start_container: %s', str(e))
