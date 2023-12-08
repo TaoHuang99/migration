745ee247096c9f212c5fdfcdfdc1c25451b54e80
Huang Tao <69231000+TaoHuang99@users.noreply.github.com>
2023-12-08 16:48:03 +0800

    Update pikesKeyPutEtcd.sh

--- pikesKeyPutEtcd.sh
+++ pikesKeyPutEtcd.sh
@@ -2,9 +2,10 @@
 
 # 声明网站域名和IP地址之间的映射关系
 declare -A domain_ip_map=(
-  [http://piskes-4.com]=192.168.122.178
-  [http://piskes-5.com]=192.168.122.84
-  [http://piskes-6.com]=192.168.122.142
+  [http://piskes-4.com]=192.168.122.218
+  [http://piskes-5.com]=192.168.122.29
+  [http://piskes-6.com]=192.168.122.47
+  [http://piskes-2.com]=192.168.122.172
 )
 
 for domain in "${!domain_ip_map[@]}"; do
